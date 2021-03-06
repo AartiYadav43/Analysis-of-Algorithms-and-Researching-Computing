8b79785ed0bf7029c05527bb9780b9e0578c82fc
AartiYadav43 <77489106+AartiYadav43@users.noreply.github.com>
2021-03-06 11:31:21 +0530

    Add files via upload

---
+++ Pract4_BucketSort.py
@@ -0,0 +1,32 @@
+def insertionSort(b):
+    for i in range(1,len(b)):
+        up=b[i]
+        j=i-1
+        while j>=0 and b[j]>up:
+            b[j+1]=b[j]
+            j-=1
+        b[j+1]=up
+    return b
+
+def bucketSort(x):
+    arr=[]
+    slot=10 
+    for i in range(slot):
+        arr.append([])
+
+    for j in x:
+        index=int(slot*j)
+        arr[index].append(j)
+
+    for i in range(slot):
+        arr[i]=insertionSort(arr[i])
+
+    k=0
+    for i in range(slot):
+        for j in range(len(arr[i])):
+            x[k]=arr[i][j]
+            k+=1
+    return x
+x=[0.897,0.565,0.656,0.1234,0.665,0.3434]
+print("Sorted Array is")
+print(bucketSort(x))
